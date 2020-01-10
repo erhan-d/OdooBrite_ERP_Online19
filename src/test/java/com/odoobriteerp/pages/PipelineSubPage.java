3442cb3c2153f9b998cfacfc7f76c3230be29c70
lbosimova14 <lbosimova14@gmail.com>
2020-01-09 21:34:23 -0500

    commiting

--- src/test/java/com/odoobriteerp/pages/PipelineSubPage.java
+++ src/test/java/com/odoobriteerp/pages/PipelineSubPage.java
@@ -3,6 +3,7 @@ package com.odoobriteerp.pages;
 public class PipelineSubPage {
     public static void main(String[] args) {
         System.out.println("hello word");
+        //hello online 12
     }
 
 
